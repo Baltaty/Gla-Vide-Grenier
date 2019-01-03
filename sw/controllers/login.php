1415766f6d2f1e71a5721688e984279f62bba6dc
JOK <jeanolivier321@gmail.com>
2019-01-03 16:05:15 +0100

    connection effectuer a bd avec sucess

--- sw/controllers/login.php
+++ sw/controllers/login.php
@@ -6,14 +6,42 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type:application/json');
 
+include '../dbconnexion.php';
+
 #... verif iic apres on mettra des tockens de connexions de sessions mais actu boff pas trop le temps
 $response = [];
 if(isset($_POST)){
     $user_login=$_POST['email'];
     $user_password =$_POST['password'];
 
+
     if($user_login== "Admin" && $user_password == "glazik"){
+
+            $bdconnect = connectionToBD();
+            $data = [];
+            try{
+                $sql = "SELECT *FROM user;";
+                $result = $bdconnect->query($sql);
+                $result->setFetchMode(PDO::FETCH_ASSOC);
+
+                foreach ($result as $item){
+                    $data [] = [
+                     "name"=>$item['name'],
+                     "prenom"=>$item['prenom'],
+                     "dateNaissance"=>$item['dateNaissance'],
+                     "civilite"=>$item['civilite'],
+                     "email"=>$item['email'],
+                     "password"=>$item['type'],
+                     "numero"=>$item['numero'],
+                    ] ;
+                }
+            }catch (PDOException $ex){
+                echo $ex->getMessage();
+                die();
+            }
+
             $response = [
+              "data"=>$data,
               "status" => "loggedin",
               "user" =>"nameOfuser",
               "message"=> " un mignon message",
--- sw/dbconnexion.php
+++ sw/dbconnexion.php
@@ -1,4 +1,26 @@
 <?php
 
-$configs_env = file_get_contents('../config.json');
-$configs_env = json_decode($configs_env,true);
+    function connectionToBD(){
+        try
+        {
+
+            $configs_env = file_get_contents('../config.json');
+            $configs_env = json_decode($configs_env,true);
+
+            $USERS_db=  $configs_env['user'];
+            $PASSWORD_db= $configs_env['password'];
+            $HOST_db= $configs_env['hostname'];
+            $NAME_db= $configs_env['dbname'];
+
+            $bdconect = new PDO("mysql:host=".$HOST_db.";dbname=".$NAME_db."",
+                $USERS_db, $PASSWORD_db, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8")
+            );
+            // $bdd = new PDO("mysql:host=".$host.";dbname=".$dbname."", $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
+            // echo 'base de donnees connectee';
+            return $bdconect;
+        }catch(Exception $e){
+            echo "Erreur: ".$e->getMessage();
+            die("<h2>Impossible de se connecter à la base de données !</h2>");
+        }
+
+    }
