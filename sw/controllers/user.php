7d03e0d5671ad37db26c27f0b818fa7045e1e4dd
JOK <jeanolivier321@gmail.com>
2019-01-04 16:30:22 +0100

    le fichier user.php est decoupe en fonction

--- sw/controllers/user.php
+++ sw/controllers/user.php
@@ -12,22 +12,57 @@ include '../dbconnexion.php';
 $response = [];
 if(isset($_POST)){
 
-//    print_r($_POST);
-    $sql ="INSERT INTO user (nom, prenom, dateNaissance, civilite, numero, email, password, typeUser)
-    VALUES (:nom, :prenom, :dateNaissance, :civilite, :numero, :email, :password, :typeUser) ";
-    $bdconnect = connectionToBD();
-    try{
-        $preapreREq = $bdconnect->prepare($sql);
-//        $preapreREq->execute($_POST);
-        sendMail("devops.integrale@gmail.com", "REGISTER GLAZIK GYM", "bienvenu".$_POST['nom']);
+    // Ajouter un utilisateur // Vendeur en occurence
+    if($_POST['action']=="ADD"){
+        $data = copyArray($_POST, "action");
+        print_r($data);
+        addUser($data);
+    }
 
-        $response =[
-            "status"=>true,
-            "message"=>"un message"
-        ];
+}
+
+
+
+
+
+
+
+// Ajout d'un utilisateur
+function addUser($data){
+
+        $sql ="INSERT INTO user (nom, prenom, dateNaissance, civilite, numero, email, password, typeUser)
+    VALUES (:nom, :prenom, :dateNaissance, :civilite, :numero, :email, :password, :typeUser) ";
+        $bdconnect = connectionToBD();
+        try{
+            $preapreREq = $bdconnect->prepare($sql);
+            $preapreREq->execute($data);
+//            sendMail($_POST['email'], "REGISTER GLAZIK MEMBER", "bienvenu".$data['nom']);
+
+            $response =[
+                "status"=>true,
+                "civilite"=>$data['civilite'],
+                "username"=>$data['nom'] ." ". $data['prenom'] ,
+                "message"=>" Votre insciption à été prise en compte avec succès, un mail de confirmation vous à été envoyé merci !"
+            ];
 
         }catch (PDOException $ex){
-        echo $ex->getMessage();
+            echo $ex->getMessage();
+        }
+        echo json_encode($response);
+}
+
+
+
+
+function copyArray($tableau, $exceptItem){
+    $toExecute = array();
+    if(is_array($tableau)){
+        foreach ($tableau as $item =>$value){
+
+            if($item !=$exceptItem) {
+                $toExecute[$item] =$value;
+            }
+        }
     }
-    echo json_encode($response);
+    return $toExecute;
 }
