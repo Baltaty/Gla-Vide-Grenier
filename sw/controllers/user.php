a8b389e79ef1fe5992bac7198d00d5cf5403ba07
JOK <jeanolivier321@gmail.com>
2019-01-04 23:00:12 +0100

    trigramme is ok

--- sw/controllers/user.php
+++ sw/controllers/user.php
@@ -15,7 +15,6 @@ if(isset($_POST)){
     // Ajouter un utilisateur // Vendeur en occurence
     if($_POST['action']=="ADD"){
         $data = copyArray($_POST, "action");
-        print_r($data);
         addUser($data);
     }
 
@@ -25,15 +24,86 @@ if(isset($_POST)){
 
 
 
+function trigrammeGenerate($data , $bdconnect){
+
+
+         $trigrammeGenerated = strtoupper($data['prenom'][0]);
+         $trigrammeGenerated = $trigrammeGenerated . "" . strtoupper(substr($data['nom'], 0, 2));
+
+         $resuldata = [];
+         $sql = "SELECT *FROM user WHERE  user.trigramme='$trigrammeGenerated' LIMIT 1";
+         $result = $bdconnect->query($sql);
+         $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        foreach ($result as $item){
+            $resuldata = [
+                "nom"=>$item['nom'],
+                "prenom"=>$item['prenom'],
+                "trigrammeExist"=>$item['trigramme'],
+            ] ;
+        }
+
+        if(!empty($resuldata)){
+            // si le trigramme exist deja
+            if($trigrammeGenerated[2] != "Z" ){
+                 return nextTrigramme($trigrammeGenerated);
+            } else {
+                return aleatoireTrigramme();
+            }
+        }else{
+            return $trigrammeGenerated;
+        }
+
+
+};
 
 
 // Ajout d'un utilisateur
 function addUser($data){
 
-        $sql ="INSERT INTO user (nom, prenom, dateNaissance, civilite, numero, email, password, typeUser)
-    VALUES (:nom, :prenom, :dateNaissance, :civilite, :numero, :email, :password, :typeUser) ";
         $bdconnect = connectionToBD();
+        $trigram = trigrammeGenerate($data, $bdconnect);
+        do {
+            $isok =  false;
+            $resuldata = [];
+            $sql = "SELECT *FROM user WHERE  user.trigramme='$trigram' LIMIT 1";
+            $result = $bdconnect->query($sql);
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+
+            foreach ($result as $item) {
+                $resuldata = [
+                    "nom" => $item['nom'],
+                    "prenom" => $item['prenom'],
+                    "trigrammeExist" => $item['trigramme'],
+                ];
+            }
+            if(!empty($resuldata)){
+                // si le trigramme exist deja
+                if($trigram[2] != "Z" ){
+                    $trigram = nextTrigramme($trigram);
+                    $isok = true;
+                } else {
+                    $trigram = aleatoireTrigramme();
+                    $isok = true;
+                }
+            }else {
+                $isok = false;
+            }
+        }while($isok);
+
+//        print_r($trigram);
+//        die();
+//
+
+         // Ajouter un trigramme dans la data;
+         $data['trigramme'] = $trigram;
+
+         
         try{
+
+            $sql =" INSERT INTO user (nom, prenom, dateNaissance, civilite, numero, email, password, typeUser, trigramme)
+                VALUES (:nom, :prenom, :dateNaissance, :civilite, :numero, :email, :password, :typeUser, :trigramme) ";
+
             $preapreREq = $bdconnect->prepare($sql);
             $preapreREq->execute($data);
 //            sendMail($_POST['email'], "REGISTER GLAZIK MEMBER", "bienvenu".$data['nom']);
@@ -42,7 +112,7 @@ function addUser($data){
                 "status"=>true,
                 "civilite"=>$data['civilite'],
                 "username"=>$data['nom'] ." ". $data['prenom'] ,
-                "message"=>" Votre insciption à été prise en compte avec succès, un mail de confirmation vous à été envoyé merci !"
+                "message"=>" Votre insciption a ete prise en compte avec succes, un mail de confirmation vous à été envoyé merci !"
             ];
 
         }catch (PDOException $ex){
@@ -53,9 +123,9 @@ function addUser($data){
 
 
 
-
+// copy un tableau a l'exeception d'un champ $exeception;
 function copyArray($tableau, $exceptItem){
-    $toExecute = array();
+    $toExecute = [];
     if(is_array($tableau)){
         foreach ($tableau as $item =>$value){
 
@@ -65,4 +135,33 @@ function copyArray($tableau, $exceptItem){
         }
     }
     return $toExecute;
+}
+
+function nextTrigramme($trigammeExistant){
+    $alphabet = [
+        "A","B","C","D","E","F","G","H","I","J","K","L","M",
+        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    ];
+    $lastPosition =strtoupper(substr($trigammeExistant,-1));
+    $index = 0;
+    foreach($alphabet as $value){
+
+        if($value == $lastPosition){
+            $trigammeExistant[2] = $alphabet[$index+1];
+        }
+        $index++;
+    }
+    return $trigammeExistant;
+}
+
+function aleatoireTrigramme(){
+    $alphabet = [
+        "A","B","C","D","E","F","G","H","I","J","K","L","M",
+        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    ];
+    $otherAleaTrigramme="";
+    for($i=0 ; $i < 2; $i++ ){
+        $otherAleaTrigramme =$otherAleaTrigramme."".$alphabet[rand(0,25)];
+    }
+    return $otherAleaTrigramme;
 }
